0501b77529d8ce35935fddeaaa91416e8c7c9642
Xavyr Moss <xmoss10@gmail.com>
2017-08-06 19:14:31 +0800

    first commit

---
+++ core_word_app.rb
@@ -0,0 +1,44 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+before do 
+  @all_text = File.read("data/all_words.txt").downcase!
+  @all_sentences = @all_text.split('.')
+  @all_core_words = File.readlines("data/core_words.txt").map! do |word|
+    word[0..-2]
+  end
+end
+
+get "/" do
+  erb :home
+end
+
+get "/search" do 
+  @compiled_sentences = find_sentences()
+  @count = get_searched_word_count()
+  erb :search
+end
+
+def get_searched_word_count
+  count = 0
+  @all_sentences.each do |sentence|
+    count += 1 if sentence.include?(@core_word)
+  end
+  count
+end
+
+def find_sentences
+  found_sentences = []
+  @core_word = params[:query]
+  @all_sentences.each do |sentence|
+    if sentence.include?(@core_word)
+      found_sentences.push(sentence)
+    end
+  end
+  found_sentences 
+end
+
+def highlight(text, term)
+  text.gsub(term, %(<strong>#{term}</strong>))
+end
